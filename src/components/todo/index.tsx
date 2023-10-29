c2fdfe976a8f0d76ea5896b5b140720abdafcc97
ergincetn01 <ergincetn01@gmail.com>
2023-10-29 13:04:59 +0300

    some styling and lineargradient added

--- src/components/todo/index.tsx
+++ src/components/todo/index.tsx
@@ -1,39 +1,33 @@
 import { useState } from 'react';
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView
-} from 'react-native';
+import { Pressable, StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Dialog, Portal, PaperProvider } from 'react-native-paper';
 import TodoList from './todoList';
 import NewTodo from './newTodo';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { resetTitle, setRandomId } from '../../store/todo';
-import { RootState } from '../../store';
+import { LinearGradient } from 'expo-linear-gradient';
 
 function TodoScreen() {
   const [visible, setVisible] = useState(false);
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
 
   const closeModal: any = () => {
     setVisible(false);
   };
   const openModal: any = () => {
     setVisible(true);
-    dispatch(resetTitle())
-    dispatch(setRandomId())
+    dispatch(resetTitle());
+    dispatch(setRandomId());
   };
 
   return (
     <PaperProvider>
       <View style={styles.main}>
-        <View style={styles.headerContainer}>
-           <View>
-          <Text style={styles.title}>Todo List</Text>
-        </View>
- <View style={styles.btnContainer}>
+        {/* <View style={styles.headerContainer}>
+          <View>
+            <Text style={styles.title}>Todo List</Text>
+          </View>
+          <View style={styles.btnContainer}>
             <Pressable onPress={openModal} style={styles.pressable}>
               <Text style={styles.btnText}>+</Text>
             </Pressable>
@@ -48,18 +42,40 @@ function TodoScreen() {
               </Dialog>
             </Portal>
           </View>
-        </View>
-       
-       
-  
- <View style={styles.bottomContainer}>
-          <View style={styles.listContainer}>
-            <TodoList />
+        </View> */}
+        <LinearGradient colors={["#662D8C","#ED1E79"]} style={styles.mainLinear}>
+        <View>
+            <Text style={styles.title}>Todo List</Text>
           </View>
-        
+          <View style={styles.btnContainer}>
+            <Pressable onPress={openModal} style={styles.pressable}>
+              <Text style={styles.btnText}>+</Text>
+            </Pressable>
+            <Portal>
+              <Dialog visible={visible} onDismiss={closeModal}>
+                <Dialog.Title>
+                  <Text>Add new todo</Text>
+                </Dialog.Title>
+                <Dialog.Content>
+                  <NewTodo onDismiss={closeModal} />
+                </Dialog.Content>
+              </Dialog>
+            </Portal>
+          </View>
+
+        </LinearGradient>
+
+
+        <View>
+          <LinearGradient
+            style={styles.bottomContainer}
+            colors={['black', '#1737eb', 'transparent']}
+          >
+            <View style={styles.listContainer}>
+              <TodoList />
+            </View>
+          </LinearGradient>
         </View>
-       
-       
       </View>
     </PaperProvider>
   );
@@ -67,15 +83,20 @@ function TodoScreen() {
 
 const styles = StyleSheet.create({
   main: {
-    backgroundColor: '#20E0E0',
-    flexDirection: "column",
+    flexDirection: 'column',
   },
+  mainLinear: { flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+height:"20%",
+  width: "100%",
+  paddingLeft: 20,
+  paddingRight: 10
+}, 
   headerContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: '12%',
-    marginHorizontal: 10
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   btnContainer: { justifyContent: 'flex-end', alignItems: 'flex-end' },
   btnText: {
@@ -91,9 +112,9 @@ const styles = StyleSheet.create({
     height: 80,
     width: 80,
     justifyContent: 'center',
-    backgroundColor: 'purple',
+    backgroundColor: '#18dd72',
   },
-  title: { textAlign: 'center', fontSize: 30, fontWeight: "bold" },
+  title: { textAlign: 'center', fontSize: 30, color: "#ffffff", fontWeight: 'bold' },
   todoButton: {
     padding: 10,
     width: '30%',
@@ -105,10 +126,9 @@ const styles = StyleSheet.create({
   bottomContainer: {
     width: '100%',
     height: '100%',
-    backgroundColor: '#0E0D0D',
-    marginTop: '5%',
+    marginTop: '-5%',
     borderTopLeftRadius: 12,
-    borderTopRightRadius: 12
+    borderTopRightRadius: 12,
   },
 });
 
--- src/components/todo/todoList.tsx
+++ src/components/todo/todoList.tsx
@@ -1,55 +1,64 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Pressable
-} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Pressable } from 'react-native';
 import { RootState } from '../../store';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteTodo } from '../../store/todo';
 function TodoList() {
-
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
 
   const currentTodos = useSelector((state: RootState) => state.todo.todos);
   const currentID = useSelector((state: RootState) => state.todo.id);
 
-  const handleDelete: any=(item:any)=> {
-    dispatch(deleteTodo(item))
-  }
+  const handleDelete: any = (item: any) => {
+    dispatch(deleteTodo(item));
+  };
 
   return (
-<View>
-  {currentTodos.length ===0 ? <Text style={styles.empty}>No todos added!</Text> :
-   <FlatList
-      style={styles.list}
-      data={currentTodos}
-      renderItem={({ item }) => (
-        <View style={styles.itemContainer} key={currentID}>
-          <Text style={styles.title}>{item.title} </Text>
-          <Pressable onPress={()=>handleDelete(item.id)} style={styles.deleteButton}>
-            <Text  style={styles.deleteText} >X</Text> 
-            </Pressable>
-        </View>
+    <View>
+      {currentTodos.length === 0 ? (
+        <Text style={styles.empty}>No todos added!</Text>
+      ) : (
+        <FlatList
+          style={styles.list}
+          data={currentTodos}
+          renderItem={({ item }) => (
+            <View style={styles.itemContainer} key={currentID}>
+              <Text style={styles.title}>{item.title} </Text>
+              <Pressable
+                onPress={() => handleDelete(item.id)}
+                style={styles.deleteButton}
+              >
+                <Text style={styles.deleteText}>X</Text>
+              </Pressable>
+            </View>
+          )}
+        />
       )}
-    />
-  
-  }
-     
-</View>
-    
-
+    </View>
   );
 }
 const styles = StyleSheet.create({
-  empty: {color: "white", fontSize: 25, textAlign: "center", marginTop: 20},
-  list: { },
-  itemContainer: { backgroundColor: 'green', flexDirection: "row", alignItems: "center", borderRadius: 12,
-paddingLeft: 10, marginVertical: 10, height: 50, justifyContent:"space-between" },
-  title: { color: 'white', fontSize: 20 },
-  deleteButton: {backgroundColor: "red", width: 50, height: 50, alignItems: "center", justifyContent: "center", borderRadius: 12},
-  deleteText: {color: "white", fontSize: 25, fontWeight: "bold"}
+  empty: { color: 'white', fontSize: 25, textAlign: 'center', marginTop: 20 },
+  list: {},
+  itemContainer: {
+    backgroundColor: '#a8e6bc',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 12,
+    paddingLeft: 10,
+    marginVertical: 10,
+    height: 50,
+    justifyContent: 'space-between',
+  },
+  title: { color: '#1737eb', fontSize: 20 },
+  deleteButton: {
+    backgroundColor: 'red',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+  },
+  deleteText: { color: 'white', fontSize: 25, fontWeight: 'bold' },
 });
 
 export default TodoList;
